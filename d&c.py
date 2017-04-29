975da2bc611ecd0aaf16f3c0df59240c67183d4d
Alex McClanahan <alexmcclanahan94@gmail.com>
2017-04-28 22:49:36 -0400

    Update d&c.py

--- d&c.py
+++ d&c.py
@@ -1,36 +1,47 @@
-# divide and conquer
-def findmid(a):
-    if len(a)%2!=0:
-        mid = (len(a)/2)-(1/2)
-    if len(a)%2==0:
-        mid = (len(a)/2)-1
-    return mid
+def midIndex(a):
+    # fxn to find midpoint of array
+    low = 0
+    high = len(a)
+    return int((high-low)/2)
+a = [-5, 10, -1, 15, 25, 13, -10]
 
-def findmax(a):
-    mid = findmid(a)
-    print mid
-    leftmax = -10000
-    sum=0
-    lefti=0
+
+def maxX(a, low, mid, high):
+    mid = midIndex(a)
+    sumL = -float('inf')
+    leftind = 0
+    tempL = 0
     for i in range(mid, -1, -1):
-        sum += a[i]
-        if leftmax<sum:
-            leftmax=sum
-            lefti=i
-    rightmax=-10000
-    sumr=0
-    righti=10
+        tempL += a[i]
+        if tempL > sumL:
+            sumL = tempL
+            leftind = i
+
+    sumR = -float('inf')
+    rightind = 0
+    tempR = 0
     for j in range(mid+1, len(a)):
-        sumr+=a[j]
-        if rightmax<sumr:
-            rightmax=sumr
-            righti=j
-    maxXsubarray = rightmax+leftmax
-    print maxXsubarray
-    print [lefti,righti]
-def fxn(a):
-    findmax(a)
+        tempR += a[j]
+        if tempR > sumR:
+            sumR = tempR
+            rightind = j
 
+    sum = sumR + sumL
+    return leftind, rightind, sum
 
-a = [-5, 1, 3, 7, -6, 16, 18, -800, 19, -500]
-fxn(a)
+def recursion(a, low, high):
+    if high - 1 == low:
+        return low, high, a[low]
+    else:
+        midd = int((high - low)/2)
+        left_low, left_high, left_sum = recursion(a, low, midd)
+        right_low, right_high, right_sum = recursion(a, midd + 1, high)
+        cross_low, cross_high, cross_sum = maxX(a, low, midd, high)
+        if left_sum > right_sum and left_sum > cross_sum:
+            return left_low, left_high, left_sum
+        elif right_sum > left_sum and right_sum > cross_sum:
+            return right_low, right_high, right_sum
+        elif cross_sum > left_sum and cross_sum > right_sum:
+            return cross_low, cross_high, cross_sum
+
+recursion(a, 0, len(a))
